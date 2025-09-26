267911204626d925f7db8e3bbf23dbf21b82c7db
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-09-26 11:01:33 +0700

    Create TPStrings.kt

---
+++ app/src/main/kotlin/org/syndes/rust/preferences/TPStrings.kt
@@ -0,0 +1,23 @@
+package org.syndes.rust
+
+object TPStrings {
+    const val EMPTY = ""
+    const val ITEM_KEY = "key"
+    const val ITEM_IMAGE = "image"
+    const val START_PATH = "START_PATH"
+    const val RESULT_PATH = "RESULT_PATH"
+    const val SELECTION_MODE = "SELECTION_MODE"
+    const val SLASH = "/"
+    const val NEW_FILE_TXT = "newfile.txt"
+    const val ACTION_VIEW = "android.intent.action.VIEW"
+    const val EN = "en"
+    const val DEFAULT = "default"
+    const val UTF_8 = "UTF-8"
+    const val STAR = "*"
+    const val VERSION_NAME = "version_name"
+    const val FILE_DIALOG = "FileDialog"
+    const val FONT_MONOSPACE = "Monospace"
+    const val FONT_SERIF = "Serif"
+    const val FONT_SANS_SERIF = "Sans Serif"
+    const val FOLDER_UP = ".."
+}
